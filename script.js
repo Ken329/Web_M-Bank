78a0bde2b890ac542920a35c395e9b8fe89d5047
ken <ken_037729@hotmail.com>
2021-01-10 15:52:15 +0800

    done login sign up page

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var divLogin = document.getElementById('login-div');
 var divSignup = document.getElementById('signup-div');
-var myBtn = document.getElementById('loginToggle');
+var buttonLeft = document.getElementById('btnLogin');
+var buttonRight = document.getElementById('btnSignUp');
 function signUp(){
-    myBtn.style.right = "220px";
+    buttonRight.style.backgroundColor = "#ED4325";
+    buttonLeft.style.backgroundColor = "#FDFBFB";
     divSignup.style.left = "150px";
     divLogin.style.left = "-450px";
     divLogin.style.display = "none";
     divSignup.style.display = "block";
 }
 function login(){
-    myBtn.style.left = "219px";
+    buttonLeft.style.backgroundColor = "#ED4325";
+    buttonRight.style.backgroundColor = "#FDFBFB";
     divSignup.style.left = "600px";
     divLogin.style.left = "150px";
     divSignup.style.display = "none";
